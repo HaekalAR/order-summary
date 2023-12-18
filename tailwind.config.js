94d09bf6751cc462d6a7097b9ec3cded0052d8ff
Haekal Abdillah Ramadhan <ekal26b@gmail.com>
2023-12-18 18:17:06 +0700

    Adding alts to img and prettier to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "r-verypaleblue": "hsl(225, 100%, 98%)",
+      "r-paleblue": "hsl(225, 100%, 94%)",
+      "r-brightblue": "hsl(245, 75%, 52%)",
+      "r-desaturatedblue": "hsl(224, 23%, 55%)",
+      "r-darkblue": "hsl(223, 47%, 23%)",
+    },
   },
   plugins: [],
-}
+};
